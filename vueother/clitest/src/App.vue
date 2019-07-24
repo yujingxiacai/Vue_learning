8b1e61ff24458410356dd440782f147646a4e67e
unknown <shenyukang@360buyad.local>
2019-07-24 10:23:16 +0800

    提交1

---
+++ vueother/clitest/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <h1>$nextTick测试</h1>
+      <h4>为了在数据变化之后等待 Vue 完成更新 DOM，可以在数据变化之后立即使用 Vue.nextTick(callback)。这样回调函数将在 DOM 更新完成后被调用</h4>
+    <Next />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Next from './components/Nexttick.vue'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Next
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vueother/clitest/src/components/Nexttick.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="nextt">
+        <div @click="Change">{{message}}</div> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: "nextt",
+    props: [],
+    data() {
+        return {
+            message: 1
+        }
+    },
+    computed: {
+        
+    },
+    watch: {
+        
+    },
+    methods: {
+        Change(){
+            this.message = 'new message';
+            // 此时并没有及时更新，等待dom完全更新后要使用$nextTick
+            console.error("27行》》》",this.$el.textContent === 'new message') // false
+            this.$nextTick(function () {
+                console.log("nextTick中，29行》》》",this.$el.textContent === 'new message') // true
+            })
+        }
+    },
+
+    // 生命周期钩子函数
+    mounted() {
+        
+    },
+    created() {
+        
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
